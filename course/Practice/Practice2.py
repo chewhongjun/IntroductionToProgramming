49f41eebd528b194884e2c05cf34e5c3e431d180
hong jun <hchew007@e.ntu.edu.sg>
2020-05-28 22:04:43 +0800

    Question 2

---
+++ course/Practice/Practice2.py
@@ -0,0 +1,13 @@
+sumInput=countInput=0                           #First we initalise 2 variables we will be using sum of the user input(sumInput) and count of user inputs(countInput)
+
+userInput = input("Enter User Input:")          #Reads in the user input
+
+
+while(userInput!='q'):                          #this loop breaks when the userInput is 'q'
+
+    sumInput = sumInput+int(userInput)          #Adds the userInput value to the sum 
+    countInput = countInput + 1                 #Increase the counter by 1
+
+    userInput=input("Enter User Input:")        #This asks for the user to enter again and the loop restarts till you hit the value 'q'
+
+print(str(countInput)+" Integers has been entered, with an sum value of "+str(sumInput))    #This is the print statement it is not complete if you can try to fix it!
